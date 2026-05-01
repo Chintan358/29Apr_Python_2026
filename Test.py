48155f32d4229a95524fe5c1c63c3d87a260a11d
chintan <chintan@gmail.com>
2026-05-01 16:37:14 +0530

    basic

---
+++ Test.py
@@ -0,0 +1,47 @@
+# #include<stdio.h>
+# #include<conio.h>
+# void main()
+# {
+#     printf("Helllo world");
+# }
+
+
+# class Demo
+# {
+#     public static void main(String args[])
+#     {
+#         System.out.println("Hello world");
+#     }
+# }
+
+# print("Hello world")
+
+# print('hello python')
+
+# print("""hello 
+#       dfdf
+#       fdf
+#       fdf
+#        python""")
+
+
+# Hello \" tops
+
+# print('Hello " tops')
+# print("Hello ' tops ")
+
+# print("He\tll\bo \" \n python")
+
+# print("Hello",end=" ")
+# print("Python")
+
+# print("python","java","php","android",sep=" | ")
+
+name = "sad"
+email = "priya@gmail.com"
+
+# print("my name is {0} and email is {1}".format(name,email))
+
+# print(f"my name is {name} and email is {email}")
+
+print(r"He\tll\bo \" \n python")
