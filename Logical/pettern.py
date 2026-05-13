fa20a1e9349c8e9a755f5f52f83e1010be7bbe29
chintan <chintan@gmail.com>
2026-05-13 18:02:47 +0530

    pattern

---
+++ Logical/pettern.py
@@ -0,0 +1,117 @@
+# *****
+# *****
+# *****
+# *****
+# *****
+# lines = 10
+# for j in range(lines):
+#     for i in range(lines):
+#         print("*",end=" ")
+#     print()
+
+
+# lines = 10
+# for j in range(lines):
+#     print("*"*lines)
+   
+# *
+# **
+# ***
+# ****
+# *****
+
+
+# lines = 5
+# for j in range(lines):
+#     for i in range(j+1):
+#         print("*",end=" ")
+#     print()
+
+
+# lines = 5
+# for j in range(lines):
+#     print("*"*(j+1))
+
+# *****
+# ****
+# ***
+# **
+# *
+
+# lines = 5
+# for j in range(lines):
+#     for i in range(lines-j):
+#         print("*",end=" ")
+#     print()
+
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+
+# lines = 5
+# for j in range(lines):
+#     for k in range(lines-(j+1)):
+#          print(" ",end=" ")
+
+#     for i in range(j+1):
+#         print("*",end=" ")
+#     print()
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+
+# lines = 5
+# for j in range(lines):
+#     for k in range(j):
+#          print(" ",end=" ")
+
+#     for i in range(lines-j):
+#         print("*",end=" ")
+#     print()
+
+    
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * * 
+
+
+
+# lines = 5
+# for j in range(lines):
+#     for k in range(lines-(j+1)):
+#          print(" ",end="")
+
+#     for i in range(j+1):
+#         print("* ",end="")
+#     print()
+
+
+#     *
+#    * *
+#   * * *
+#  * * * *
+# * * * * * 
+#  * * * *
+#   * * *
+#    * *
+#     *
+
+
+#     *
+#    * *
+#   *   *
+#  *     *
+# *       * 
+#  *     *
+#   *   *
+#    * *
+#     *
