398e138886caf5d474b5ad2d99e91aadef1b3432
chintan <chintan@gmail.com>
2026-05-20 17:24:06 +0530

    dictionary

--- collections/DictionaryDemo.py
+++ collections/DictionaryDemo.py
@@ -1,17 +1,85 @@
-# student = {"name":"mithilesh","email":"mithilesh@gmail.com"}
+# student = {
+#     "name":"mithilesh",
+#     "email":"mithilesh@gmail.com",
+#     "age":20
+#     }
 
+
+# print(student['name'])
+# print(student.get("name1"))
+
+# print(student.keys())
+# print(student.values())
+# print(student.items())
+
+# print(student['name'])
+# student['name1']="xyz"
 # print(student)
 
-# l = []
-# print(type(l))
-# t = ()
-# print(type(t))
-# d = {}
-# print(type(d))
-# s = set()
-# print(type(s))
+# st = {"phone":"968748585","gender":"male"}
+# student.update({"phone":"9685748596"})
+# student.update(st)
 
-l = [10]
-print(type(l))
-t = (10,)
-print(type(t))
+# student.pop("name")
+# student.popitem()
+# student.clear()
+# del student['name']
+# del student
+# print(student)
+
+
+
+country = {
+    "India":"IN",
+    "USA":"US",
+    "CANADA":"CAN"
+}
+
+# for i,j in country.items():
+#     print(i,j)
+
+# for i in country.keys():
+#     print(i)
+
+# k  = country.copy()
+# k = dict(country)
+# print(k)
+
+
+# user = {
+#     "name":"Priya",
+#     "email":"priya@gmail.com",
+#     "address": {
+#         "country":"India",
+#         "state":"gujarat",
+#         "city":"surat"
+#     }
+# }
+
+# print(user['address']['country'])
+
+# x = ('key1', 'key2', 'key3')
+# y = 0
+
+# thisdict = dict.fromkeys(x, y)
+
+# print(thisdict)
+
+# a = ("k1","k2","k3")
+# b = ("v1","v2","v3")
+
+# k = zip(a,b)
+# print(dict(k))
+# print(list(k))
+
+
+
+user = {
+    "name":"Priya",
+    "email":"priya@gmail.com",
+}
+
+# user['name'] = 25
+user.setdefault("name",25)
+
+print(user)
